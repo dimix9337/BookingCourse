8c233633d1ae7f1808eb9de4f3e7728b6e3a7e71
dimix <dimix9337@gmail.com>
2020-05-27 01:10:53 +0300

    test

---
+++ resources/js/bookable/Bookable.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="row">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+                    <h2>{{ bookable.title }}</h2>
+                    <hr/>
+                    <article>{{ bookable.description }}</article>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            availability & prices
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            bookable: null
+        }
+    },
+    created() {
+        axios.get(`/api/bookable/${this.$route.params.id}`).then(response => (this.bookable = response.data));
+    },
+}
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,23 @@
+import VueRouter from "vue-router";
+import Bookables from "./bookables/Bookables";
+import Bookable from './bookable/Bookable';
+
+const routes = [
+    {
+        path: "/",
+        component: Bookables,
+        name: "home",
+    },
+    {
+        path: "/bookable/:id",
+        component: Bookable,
+        name: "bookable",
+    },
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+});
+
+export default router;
