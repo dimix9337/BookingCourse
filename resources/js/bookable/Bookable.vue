b06109168d9abf2a45ed29db6ef9ddec777a3c81
Vlad Abapolov <v.abapolov@newlinetechnologies.net>
2020-06-21 13:48:31 +0300

    Fixes

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -3,27 +3,42 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-body">
-                    <h2>{{ bookable.title }}</h2>
-                    <hr/>
-                    <article>{{ bookable.description }}</article>
+                    <div v-if="!loading">
+                        <h2>{{ bookable.title }}</h2>
+                        <hr/>
+                        <article>{{ bookable.description }}</article>
+                    </div>
+                    <div v-else>
+                        Data is loading...
+                    </div>
                 </div>
             </div>
         </div>
         <div class="col-md-4">
-            availability & prices
+            <availability></availability>
         </div>
     </div>
 </template>
 
 <script>
+import Availability from "./Availability";
+
 export default {
+    components: {
+        Availability
+    },
     data() {
         return {
-            bookable: null
+            bookable: null,
+            loading: false
         }
     },
     created() {
-        axios.get(`/api/bookable/${this.$route.params.id}`).then(response => (this.bookable = response.data));
+        this.loading = true;
+        axios.get(`/api/bookable/${this.$route.params.id}`).then(response => {
+            this.bookable = response.data;
+            this.loading = false;
+            });
     },
 }
 </script>
