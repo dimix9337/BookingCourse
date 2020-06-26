ebc9212d09514fcd18137bfa89a7eb6fa2031e1a
Vlad Abapolov <v.abapolov@newlinetechnologies.net>
2020-06-26 22:06:56 +0300

    lesson_63

---
+++ resources/js/bookable/Availability.vue
@@ -0,0 +1,94 @@
+<template>
+    <div>
+        <h6 class="text-uppercase text-secondary font-weight-bolder">Chech Availability</h6>
+
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="from">From</label>
+                <input 
+                    type="text" 
+                    name="from" 
+                    class="form-control form-control-sm"
+                    placeholder="Start date" 
+                    v-model="from"
+                    @keyup.enter="check"
+                    :class="[{'is-invalid': this.errorFor('from')}]"
+                />
+            </div>
+            <div class="form-group col-md-6">
+                <label for="to">To</label>
+                <input 
+                    type="text" 
+                    name="to" 
+                    class="form-control form-control-sm"
+                    placeholder="End date"
+                    v-model="to"
+                    @keyup.enter="check"
+                    :class="[{'is-invalid': this.errorFor('to')}]"
+                />
+            </div>
+        </div>
+        <button class="btn btn-secondary btn-block" @click="check" :disabled="loading">Check!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            from: null,
+            to: null,
+            loading: false,
+            status: null,
+            errors: null
+        }
+    },
+    methods: {
+        check(){
+            this.loading = true;
+            this.errors = null;
+            
+            axios
+                .get(`/api/bookables/${this.$route.params.id}/availability?from=${this.from}&to=${this.to}`
+                )
+                .then(response => {
+                    this.status = response.status;
+                })
+                .catch(error => {
+                    if (422 === error.response.status) {
+                        this.errors = error.response.data.errors;
+                    }
+
+                    this.status = error.response.status;
+                })
+                .then( () => (this.loading = false));
+        },
+        errorFor(field) {
+            return this.hasErrors && this.errors[field] ? this.errors[field] : null;
+        }
+    },
+    computed: {
+        hasErrors() {
+            console.log('422');
+            return 422 === this.status && this.errors === null;
+        },
+        hasAvailability() {
+            console.log('200');
+            return 200 === this.status;
+        },
+        noAvailability() {
+            console.log('400');
+            return 400 === this.status;
+        }
+    },
+}
+</script>
+
+<style scoped>
+    label {
+        font-size: 0.7rem;
+        text-transform: uppercase;
+        color: gray;
+        font-weight: bolder;
+    }
+</style>
