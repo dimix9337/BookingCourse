fedec0d1e185281426125a2e19dc4055b9cb5651
Vlad Abapolov <v.abapolov@newlinetechnologies.net>
2020-06-30 01:59:02 +0300

    lesson_73_2

---
+++ resources/js/review/Review.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label class="text-muted">Select the star rating (1 is worst - 5 is best)</label>
+            <star-rating 
+                class="fa-3x" 
+                v-model="review.rating"
+            ></star-rating>
+        </div>
+        <div class="form-group">
+            <label for="content" class="text-muted">Desctibe your expitience with</label>
+            <textarea name="content" cols="30" rows="10" class="form-control"></textarea>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            review:{
+                rating: 5,
+                content: null
+            }
+        }
+    }
+    // methods: {
+    //     onRatingChanged(rating) {
+    //         console.log(rating);
+    //     }
+    // }
+}
+</script>
